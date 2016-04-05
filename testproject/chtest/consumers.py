6ccb528f538617a97db24c11f9019f9b5d319c59
Benjamin Bach <benjaoming@gmail.com>
2016-04-05 16:02:32 +0200

    Fix 'chtest' app to use 'text' key for Group messages, add better logging messages

--- testproject/chtest/consumers.py
+++ testproject/chtest/consumers.py
@@ -12,11 +12,16 @@ def ws_connect(message):
     logger.debug("Received ws connect")
     path = message.content['path']
     if path.endswith("group/"):
+        logger.debug("Connect request for group channel, adding to group channel")
         Group(TEST_GROUP).add(message.reply_channel)
 
 
 def ws_disconnect(message):
     logger.debug("Received ws disconnect")
+    path = message.content['path']
+    if path.endswith("group/"):
+        logger.debug("Disconnect request for group channel, removing from group channel")
+        Group(TEST_GROUP).discard(message.reply_channel)
 
 
 def ws_message(message):
@@ -29,10 +34,10 @@ def ws_message(message):
         logger.debug("Message is on group channel, going to send a response to the group...")
         message.reply_channel.send({'content': 'Message received on group channel, sening response to group'})
         Group(TEST_GROUP).send({
-            'content': "Message to group: {}".format(
-                message.content
-            )
+            'text': message.content['text']
         })
     else:
         logger.debug("Message received on echo channel, sending echo..")
-        message.reply_channel.send(message.content)
+        message.reply_channel.send(
+            {'text': message.content['text']}
+        )
