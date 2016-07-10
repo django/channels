16c3778f369072a770b44388fd25d787df32209c
Robert Roskam <raiderrobert@gmail.com>
2016-07-10 16:14:39 -0400

    Changed regular wsgi to point to channels settings

--- testproject/testproject/wsgi.py
+++ testproject/testproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings.channels")
 
 application = get_wsgi_application()
