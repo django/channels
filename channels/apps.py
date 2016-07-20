6f84831a4abb3a328f834c22a1e156cccee06884
AlexejStukov <sergej.morokin@googlemail.com>
2016-07-20 10:46:49 +0200

    Removed usage of removed method

--- channels/apps.py
+++ channels/apps.py
@@ -20,5 +20,3 @@ class ChannelsConfig(AppConfig):
         # Do django monkeypatches
         from .hacks import monkeypatch_django
         monkeypatch_django()
-        # Instantiate bindings
-        BindingMetaclass.register_all()
