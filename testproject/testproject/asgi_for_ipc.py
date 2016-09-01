d5a6f96f1ff19387258fb3a2357b158e7a92a213
Robert Roskam <raiderrobert@gmail.com>
2016-08-31 22:03:39 -0400

    changed back

--- testproject/testproject/asgi_for_ipc.py
+++ testproject/testproject/asgi_for_ipc.py
@@ -3,7 +3,4 @@ from channels.asgi import get_channel_layer
 import asgi_ipc
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings.channels_ipc")
-channel_layer = asgi_ipc.IPCChannelLayer(
-    prefix="test",
-    channel_memory=200 * 1024 * 1024,
-)
+channel_layer = get_channel_layer()
