994581e133e7822a87cdd3ad6e89c491508e8b43
Artem Malyshev <proofit404@gmail.com>
2017-01-23 02:00:06 +0300

    Allow to pass test label to the test runner.

--- runtests.py
+++ runtests.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
-    failures = test_runner.run_tests(["channels.tests"])
+    failures = test_runner.run_tests(sys.argv[1:] or ["channels.tests"])
     sys.exit(bool(failures))
