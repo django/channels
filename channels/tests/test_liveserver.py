cf814b151ed30dec95689e895c0215f79d7e37ea
Artem Malyshev <proofit404@gmail.com>
2017-01-23 04:46:38 +0300

    Handle ping pong message in the live server test.
    websocket.receive should point to some consumer not to the null_handler.

--- channels/tests/test_liveserver.py
+++ channels/tests/test_liveserver.py
@@ -1,15 +1,24 @@
 import websocket
 
-from channels.tests import ChannelLiveServerTestCase
+from channels.routing import route
+from channels.tests import ChannelLiveServerTestCase, apply_routes
 
 
+def ping_pong(message):
+    assert message.content['text'] == 'ping'
+    message.reply_channel.send({'text': 'pong'})
+
+
+@apply_routes([
+    route('websocket.receive', ping_pong),
+])
 class WebSocketTest(ChannelLiveServerTestCase):
     """Send and receive messages over WebSocket."""
 
     def test_send_recv(self):
 
         ws = websocket.create_connection(self.live_server_ws_url)
-        ws.send('test')
+        ws.send('ping')
         response = ws.recv()
         ws.close()
-        self.assertEqual('test', response)
+        self.assertEqual('pong', response)
