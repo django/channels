a22a348155a3073aafc94b32043da7e1046f2657
Maik Hoepfel <m@maikhoepfel.de>
2017-01-30 17:39:48 +0100

    Simplify tox and Travis configuration
    I hopefully simplified the tox configuration by following what I did in
    the other four Channels projects.
    
    I then had a good look at tox-travis and decided to remove it. It does
    add a layer of indirection with, IMHO, not enough gain. To understand
    what Travis is doing, one would need to consider two files (and
    understand tox-travis) instead of just one file. It also introduces
    another point of failure. What pushed me over was that there's a bug
    with env matching (https://github.com/ryanhiebert/tox-travis/issues/55)
    and tox or tox-travis seem to mask an Exception
    (https://travis-ci.org/django/channels/jobs/195950971#L195) that would
    be hard to debug.
    The draw back is that we duplicate the Django dependency matrix, and the
    commands that are executed in Travis and tox. We could add a "--with-qa"
    flag to runtests.py to have it execute flake8 and isort to rectify the
    latter.
    
    I extracted test dependencies as I did for asgi_redis.

--- setup.py
+++ setup.py
@@ -16,6 +16,9 @@ setup(
         'asgiref>=1.0.0',
         'daphne>=1.0.0',
     ],
+    extras_require={
+        'tests': ['coverage', 'mock', 'tox', 'flake8>=2.0,<3.0', 'isort']
+    },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
