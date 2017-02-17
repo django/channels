3e961e731173b45fbe385620896447baf4d00569
Artem Malyshev <proofit404@gmail.com>
2017-02-17 04:13:47 +0300

    Migrate live server to the multiprocessing.

--- channels/test/liveserver.py
+++ channels/test/liveserver.py
@@ -1,55 +1,44 @@
-import threading
+import multiprocessing
 
 from daphne.server import Server
 from django.test.testcases import LiveServerTestCase
-from twisted.internet import reactor
 
 from .. import DEFAULT_CHANNEL_LAYER
 from ..asgi import channel_layers
 from ..worker import Worker
-from .base import ChannelTestCaseMixin
+
+DEFAULT_CHANNEL_LAYER = 'ipc'  # FIXME: override in test
 
 
-# TODO: Is ready event for this thread.
+# TODO: Is ready event for this process.
 #
 # TODO: What we need to do in the case of multiple channel layers?
-class WorkerThread(threading.Thread):
+class WorkerProcess(multiprocessing.Process):
 
     def run(self):
 
         channel_layers[DEFAULT_CHANNEL_LAYER].router.check_default()
         self.worker = Worker(
             channel_layer=channel_layers[DEFAULT_CHANNEL_LAYER],
-            signal_handlers=False,
         )
         self.worker.ready()
         self.worker.run()
 
-    def terminate(self):
-
-        if hasattr(self, 'worker'):
-            self.worker.termed = True
-
 
 # TODO: `self.connections_override` should be processed same way as in
 # regular LiveServerTestCase.
-class LiveServerThread(threading.Thread):
+class LiveServerProcess(multiprocessing.Process):
 
-    def __init__(self,
-                 host,
-                 possible_ports,
-                 static_handler,
-                 connections_override=None):
+    def __init__(self, host, possible_ports, static_handler,
+                 connections_override, port_storage, is_ready):
 
         self.host = host
         self.possible_ports = possible_ports
         self.static_handler = static_handler
         self.connections_override = connections_override
-
-        self.error = None
-        self.is_ready = threading.Event()
-        self.worker_thread = WorkerThread()
-        super(LiveServerThread, self).__init__()
+        self.port_storage = port_storage
+        self.is_ready = is_ready
+        super(LiveServerProcess, self).__init__()
 
     def run(self):
 
@@ -61,19 +50,12 @@ class LiveServerThread(threading.Thread):
                     'tcp:interface=%s:port=%d' % (self.host,
                                                   self.possible_ports[0])
                 ],
-                signal_handlers=False,
             )
-            self.port = self.possible_ports[0]
+            self.port_storage.value = self.possible_ports[0]
             self.is_ready.set()
             self.server.run()
         except Exception as e:
-            self.error = e
             self.is_ready.set()
-
-    def terminate(self):
-
-        if hasattr(self, 'server') and reactor.running:
-            reactor.stop()
 
 
 class LiveServerPool(object):
@@ -84,20 +66,18 @@ class LiveServerPool(object):
                  static_handler,
                  connections_override=None):
 
-        self.server_thread = LiveServerThread(
-            host,
-            possible_ports,
-            static_handler,
-            connections_override,
-        )
-        self.host = self.server_thread.host
-        self.is_ready = self.server_thread.is_ready
-        self.worker_thread = WorkerThread()
+        self.host = host
+        self.port_storage = multiprocessing.Value('i')
+        self.is_ready = multiprocessing.Event()
+        self.server_process = LiveServerProcess(
+            host, possible_ports, static_handler, connections_override,
+            self.port_storage, self.is_ready)
+        self.worker_process = WorkerProcess()
 
     @property
     def port(self):
 
-        return self.server_thread.port
+        return self.port_storage.value
 
     @property
     def daemon(self):
@@ -107,31 +87,36 @@ class LiveServerPool(object):
     @daemon.setter
     def daemon(self, value):
 
-        self.server_thread.daemon = value
-        self.worker_thread.daemon = value
+        self.server_process.daemon = value
+        self.worker_process.daemon = value
 
     def start(self):
 
-        self.server_thread.start()
-        self.worker_thread.start()
+        self.server_process.start()
+        self.worker_process.start()
 
     @property
     def error(self):
 
-        return self.server_thread.error
+        error = self.server_process.exitcode
+        if error:
+            return Exception('Daphne process exitcode is %d' % error)
+        error = self.worker_process.exitcode
+        if error:
+            return Exception('Worker process exitcode is %d' % error)
 
     def terminate(self):
 
-        self.server_thread.terminate()
-        self.worker_thread.terminate()
+        self.server_process.terminate()
+        self.worker_process.terminate()
 
     def join(self):
 
-        self.server_thread.join()
-        self.worker_thread.join()
+        self.server_process.join()
+        self.worker_process.join()
 
 
-class ChannelLiveServerTestCase(ChannelTestCaseMixin, LiveServerTestCase):
+class ChannelLiveServerTestCase(LiveServerTestCase):
 
     @property
     def live_server_ws_url(self):
@@ -140,9 +125,5 @@ class ChannelLiveServerTestCase(ChannelTestCaseMixin, LiveServerTestCase):
 
     @classmethod
     def _create_server_thread(cls, host, possible_ports, connections_override):
-        return LiveServerPool(
-            host,
-            possible_ports,
-            cls.static_handler,
-            connections_override=connections_override,
-        )
+        return LiveServerPool(host, possible_ports, cls.static_handler,
+                              connections_override)
--- tests/test_liveserver.py
+++ tests/test_liveserver.py
@@ -4,14 +4,25 @@ from channels.routing import route
 from channels.test import ChannelLiveServerTestCase, apply_routes
 
 
+def connect(message):
+
+    message.reply_channel.send({'accept': True})
+
+
 def ping_pong(message):
+
     assert message.content['text'] == 'ping'
     message.reply_channel.send({'text': 'pong'})
 
 
-@apply_routes([
-    route('websocket.receive', ping_pong),
-])
+ping_pong_routing = apply_routes(
+    [route('websocket.connect', connect),
+     route('websocket.receive', ping_pong)],
+    aliases=['ipc'],
+)
+
+
+@ping_pong_routing
 class WebSocketTest(ChannelLiveServerTestCase):
     """Send and receive messages over WebSocket."""
 
@@ -22,3 +33,15 @@ class WebSocketTest(ChannelLiveServerTestCase):
         response = ws.recv()
         ws.close()
         self.assertEqual('pong', response)
+
+
+@ping_pong_routing
+class SecondWebSocketTest(ChannelLiveServerTestCase):
+    """
+    This class do the same as previous ones.  We made this to be sure
+    we can start and stop live server several times.  Live server
+    depends on twisted reactor which can be run only once during
+    program live circle.
+    """
+
+    test_send_recv = WebSocketTest.test_send_recv
