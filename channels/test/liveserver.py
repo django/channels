9368f10412e0cfa00e9c47a0148732d5ebd0be0e
Artem Malyshev <proofit404@gmail.com>
2017-03-06 05:37:51 +0300

    Run Daphne and Worker processes for each test.

--- channels/test/liveserver.py
+++ channels/test/liveserver.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import threading
 
 from daphne.server import Server
 from django.test.testcases import LiveServerTestCase
@@ -7,38 +8,42 @@ from .. import DEFAULT_CHANNEL_LAYER
 from ..asgi import channel_layers
 from ..worker import Worker
 
-DEFAULT_CHANNEL_LAYER = 'ipc'  # FIXME: override in test
-
-
-# TODO: Is ready event for this process.
-#
 # TODO: What we need to do in the case of multiple channel layers?
+
+
 class WorkerProcess(multiprocessing.Process):
+
+    def __init__(self, is_ready):
+
+        self.is_ready = is_ready
+        super(WorkerProcess, self).__init__()
+        self.daemon = True
 
     def run(self):
 
-        channel_layers[DEFAULT_CHANNEL_LAYER].router.check_default()
-        self.worker = Worker(
-            channel_layer=channel_layers[DEFAULT_CHANNEL_LAYER],
-        )
-        self.worker.ready()
-        self.worker.run()
+        try:
+            channel_layers[DEFAULT_CHANNEL_LAYER].router.check_default()
+            self.worker = Worker(
+                channel_layer=channel_layers[DEFAULT_CHANNEL_LAYER],
+            )
+            self.worker.ready()
+            self.is_ready.set()
+            self.worker.run()
+        except Exception:
+            self.is_ready.set()
+            raise
 
 
-# TODO: `self.connections_override` should be processed same way as in
-# regular LiveServerTestCase.
 class LiveServerProcess(multiprocessing.Process):
 
-    def __init__(self, host, possible_ports, static_handler,
-                 connections_override, port_storage, is_ready):
+    def __init__(self, host, possible_ports, port_storage, is_ready):
 
         self.host = host
         self.possible_ports = possible_ports
-        self.static_handler = static_handler
-        self.connections_override = connections_override
         self.port_storage = port_storage
         self.is_ready = is_ready
         super(LiveServerProcess, self).__init__()
+        self.daemon = True
 
     def run(self):
 
@@ -54,76 +59,77 @@ class LiveServerProcess(multiprocessing.Process):
             self.port_storage.value = self.possible_ports[0]
             self.is_ready.set()
             self.server.run()
-        except Exception as e:
+        except Exception:
             self.is_ready.set()
+            raise
 
 
-class LiveServerPool(object):
+class LiveServerStub(object):
 
-    def __init__(self,
-                 host,
-                 possible_ports,
-                 static_handler,
-                 connections_override=None):
+    def __init__(self, host, possible_ports):
 
         self.host = host
-        self.port_storage = multiprocessing.Value('i')
-        self.is_ready = multiprocessing.Event()
-        self.server_process = LiveServerProcess(
-            host, possible_ports, static_handler, connections_override,
-            self.port_storage, self.is_ready)
-        self.worker_process = WorkerProcess()
-
-    @property
-    def port(self):
-
-        return self.port_storage.value
-
-    @property
-    def daemon(self):
-
-        raise RuntimeError('You can use setter only on daemon property')
-
-    @daemon.setter
-    def daemon(self, value):
-
-        self.server_process.daemon = value
-        self.worker_process.daemon = value
+        self.possible_ports = possible_ports
+        self.is_ready = threading.Event()
 
     def start(self):
 
-        self.server_process.start()
-        self.worker_process.start()
+        self.is_ready.set()
 
-    @property
-    def error(self):
-
-        error = self.server_process.exitcode
-        if error:
-            return Exception('Daphne process exitcode is %d' % error)
-        error = self.worker_process.exitcode
-        if error:
-            return Exception('Worker process exitcode is %d' % error)
+    error = None
 
     def terminate(self):
 
-        self.server_process.terminate()
-        self.worker_process.terminate()
+        pass
 
     def join(self):
 
-        self.server_process.join()
-        self.worker_process.join()
+        pass
 
 
 class ChannelLiveServerTestCase(LiveServerTestCase):
 
     @property
+    def live_server_url(self):
+
+        return 'http://%s:%s' % (self._server_process.host,
+                                 self._port_storage.value)
+
+    @property
     def live_server_ws_url(self):
-        return 'ws://%s:%s' % (self.server_thread.host,
-                               self.server_thread.port)
+
+        return 'ws://%s:%s' % (self._server_process.host,
+                               self._port_storage.value)
 
     @classmethod
     def _create_server_thread(cls, host, possible_ports, connections_override):
-        return LiveServerPool(host, possible_ports, cls.static_handler,
-                              connections_override)
+
+        return LiveServerStub(host, possible_ports)
+
+    def _pre_setup(self):
+
+        super(ChannelLiveServerTestCase, self)._pre_setup()
+        self._port_storage = multiprocessing.Value('i')
+
+        server_ready = multiprocessing.Event()
+        self._server_process = LiveServerProcess(
+            self.server_thread.host,
+            self.server_thread.possible_ports,
+            self._port_storage,
+            server_ready,
+        )
+        self._server_process.start()
+        server_ready.wait()
+
+        worker_ready = multiprocessing.Event()
+        self._worker_process = WorkerProcess(worker_ready)
+        self._worker_process.start()
+        worker_ready.wait()
+
+    def _post_teardown(self):
+
+        self._server_process.terminate()
+        self._server_process.join()
+        self._worker_process.terminate()
+        self._worker_process.join()
+        super(ChannelLiveServerTestCase, self)._post_teardown()
