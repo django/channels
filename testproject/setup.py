29291e6fb47f53d286c22920f934fc7e7ee83795
Artem Malyshev <proofit404@gmail.com>
2017-03-13 19:59:52 +0300

    Add test project to the benchmark package.

--- testproject/setup.py
+++ testproject/setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='channels-benchmark',
+    packages=find_packages(),
     py_modules=['benchmark'],
     install_requires=['autobahn', 'Twisted'],
 )
