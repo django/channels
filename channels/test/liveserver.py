a488b4b83f22dc490fdae59bb16b25f532abd483
Artem Malyshev <proofit404@gmail.com>
2017-03-14 05:22:16 +0300

    Bind zero port.
    Remove port guessing like current Django master.

--- channels/test/liveserver.py
+++ channels/test/liveserver.py
@@ -1,13 +1,11 @@
 import multiprocessing
-import os
-import sys
 
 import django
 from daphne.server import Server
 from django.core.exceptions import ImproperlyConfigured
 from django.test.testcases import TransactionTestCase
 from django.test.utils import modify_settings, override_settings
-from django.utils import six
+from twisted.internet import reactor
 
 from .. import DEFAULT_CHANNEL_LAYER
 from ..asgi import ChannelLayerManager
@@ -72,11 +70,10 @@ class WorkerProcess(multiprocessing.Process):
 
 class DaphneProcess(multiprocessing.Process):
 
-    def __init__(self, host, possible_ports, port_storage, is_ready,
-                 overridden_settings, modified_settings):
+    def __init__(self, host, port_storage, is_ready, overridden_settings,
+                 modified_settings):
 
         self.host = host
-        self.possible_ports = possible_ports
         self.port_storage = port_storage
         self.is_ready = is_ready
         self.overridden_settings = overridden_settings
@@ -91,27 +88,32 @@ class DaphneProcess(multiprocessing.Process):
                 django.setup(set_prefix=False)
             else:
                 django.setup()
+
             if self.overridden_settings:
                 overridden = override_settings(**self.overridden_settings)
                 overridden.enable()
+
             if self.modified_settings:
                 modified = modify_settings(self.modified_settings)
                 modified.enable()
 
             channel_layers = ChannelLayerManager()
-            # FIXME: process all possible ports, exit after first success.
-            host, port = self.host, self.possible_ports[0]
             self.server = Server(
                 channel_layer=channel_layers[DEFAULT_CHANNEL_LAYER],
-                endpoints=['tcp:interface=%s:port=%d' % (host, port)],
+                endpoints=['tcp:interface=%s:port=0' % (self.host)],
                 signal_handlers=False,
             )
-            self.port_storage.value = self.possible_ports[0]
-            self.is_ready.set()
+            reactor.callLater(0.5, self.resolve_port)
             self.server.run()
         except Exception:
             self.is_ready.set()
             raise
+
+    def resolve_port(self):
+
+        port = self.server.ports[0].result.getHost().port
+        self.port_storage.value = port
+        self.is_ready.set()
 
 
 class ChannelLiveServerTestCase(TransactionTestCase):
@@ -122,6 +124,7 @@ class ChannelLiveServerTestCase(TransactionTestCase):
     instead of the built-in dummy client.
     """
 
+    host = 'localhost'
     ProtocolServerProcess = DaphneProcess
     WorkerProcess = WorkerProcess
     worker_threads = 1
@@ -129,14 +132,12 @@ class ChannelLiveServerTestCase(TransactionTestCase):
     @property
     def live_server_url(self):
 
-        return 'http://%s:%s' % (self._server_process.host,
-                                 self._port_storage.value)
+        return 'http://%s:%s' % (self.host, self._port_storage.value)
 
     @property
     def live_server_ws_url(self):
 
-        return 'ws://%s:%s' % (self._server_process.host,
-                               self._port_storage.value)
+        return 'ws://%s:%s' % (self.host, self._port_storage.value)
 
     def _pre_setup(self):
 
@@ -151,35 +152,9 @@ class ChannelLiveServerTestCase(TransactionTestCase):
         super(ChannelLiveServerTestCase, self)._pre_setup()
         self._port_storage = multiprocessing.Value('i')
 
-        # NOTE: Copypasted from Django's LiveServerTestCase.
-        specified_address = os.environ.get(
-            'DJANGO_LIVE_TEST_SERVER_ADDRESS',
-            'localhost:8081',
-        )
-        possible_ports = []
-        try:
-            host, port_ranges = specified_address.split(':')
-            for port_range in port_ranges.split(','):
-                extremes = list(map(int, port_range.split('-')))
-                assert len(extremes) in [1, 2]
-                if len(extremes) == 1:
-                    possible_ports.append(extremes[0])
-                else:
-                    for port in range(extremes[0], extremes[1] + 1):
-                        possible_ports.append(port)
-        except Exception:
-            msg = 'Invalid address ("%s") for live server.' % specified_address
-            six.reraise(
-                ImproperlyConfigured,
-                ImproperlyConfigured(msg),
-                sys.exc_info()[2],
-            )
-        # NOTE: End of copypasta.
-
         server_ready = multiprocessing.Event()
         self._server_process = self.ProtocolServerProcess(
-            host,
-            possible_ports,
+            self.host,
             self._port_storage,
             server_ready,
             self._overridden_settings,
