059bdc045aab36bfa41ed5b14a6dd4e447115738
Artem Malyshev <proofit404@gmail.com>
2017-03-15 06:03:07 +0300

    Depends on statistics backport on Python2.

--- testproject/setup.py
+++ testproject/setup.py
@@ -4,5 +4,9 @@ setup(
     name='channels-benchmark',
     packages=find_packages(),
     py_modules=['benchmark'],
-    install_requires=['autobahn', 'Twisted'],
+    install_requires=[
+        'autobahn',
+        'Twisted',
+        'statistics ; python_version < "3.0"',
+    ],
 )
