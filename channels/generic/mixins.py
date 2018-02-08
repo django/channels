84b4e37379fd5b62a157be10bfc71c16aa448bd1
Sven Auhagen <Sven.Auhagen@voleatech.de>
2018-02-08 13:52:41 +0100

    Permissions Mixins

---
+++ channels/generic/mixins.py
@@ -0,0 +1,43 @@
+class PermissionsMixin(object):
+    """
+    Mixin to check if user has the correct permissions
+    """
+    perm = 'consumer.changeme'
+
+    def websocket_connect(self, message):
+        if self.has_permission():
+            return super().websocket_connect(message)
+        
+        return self.close(code=-1)
+
+    def has_permission(self):
+        '''
+        Check for connect permissions
+        '''
+        if self.perm:
+            if self.scope["user"] and self.scope["user"].is_authenticated and self.scope["user"].has_perms(self.perm):
+                return True
+
+        return False
+
+class AsyncPermissionsMixin(object):
+    """
+    Mixin to check if user has the correct permissions
+    """
+    perm = 'consumer.changeme'
+
+    async def websocket_connect(self, message):
+        if self.has_permission():
+            return await super().websocket_connect(message)
+        
+        return await self.close(code=-1)
+
+    def has_permission(self):
+        '''
+        Check for connect permissions
+        '''
+        if self.perm:
+            if self.scope["user"] and self.scope["user"].is_authenticated and self.scope["user"].has_perms(self.perm):
+                return True
+
+        return False
