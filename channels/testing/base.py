80195a1ed47ab719c8144997b883ad9b44cbccb9
Daniel <33256939+dgilge@users.noreply.github.com>
2018-03-14 05:00:51 +0100

    Add logout for convenience

--- channels/testing/base.py
+++ channels/testing/base.py
@@ -3,18 +3,18 @@ from importlib import import_module
 from asgiref.testing import ApplicationCommunicator
 from django.conf import settings
 
-from ..auth import login
+from ..auth import login, logout
 from ..db import database_sync_to_async
 
 
 class AuthCommunicator(ApplicationCommunicator):
     """
-    ApplicationCommunicator subclass that logs in if scope has a user.
+    ApplicationCommunicator subclass with authentication helpers.
     """
 
     async def login(self):
         """
-        Logs in if scope has a user.
+        Logs a user in if found in the scope.
         """
         if self.scope["user"]:
             # The session might have been set by the SessionMiddleware
@@ -23,3 +23,9 @@ class AuthCommunicator(ApplicationCommunicator):
                 self.scope["session"] = engine.SessionStore()
             await login(self.scope, self.scope["user"])
             await database_sync_to_async(self.scope["session"].save)()
+
+    async def logout(self):
+        """
+        Logs a scope's user out.
+        """
+        await logout(self.scope)
