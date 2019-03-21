761481f83932e239a45569cb234e1b534da1a93f
Carlton Gibson <carlton.gibson@noumenal.es>
2019-03-21 21:40:52 +0100

    Remove body implementation.
    No longer needed.

--- channels/http.py
+++ channels/http.py
@@ -122,22 +122,6 @@ class AsgiRequest(http.HttpRequest):
         # Other bits
         self.resolver_match = None
 
-    @property
-    def body(self):
-        if not hasattr(self, "_body"):
-            # Limit the maximum request data size that will be handled in-memory.
-            if (
-                settings.DATA_UPLOAD_MAX_MEMORY_SIZE is not None
-                and self._content_length > settings.DATA_UPLOAD_MAX_MEMORY_SIZE
-            ):
-                raise RequestDataTooBig(
-                    "Request body exceeded settings.DATA_UPLOAD_MAX_MEMORY_SIZE."
-                )
-            self._body = self.read()
-            self._stream = BytesIO(self._body)
-
-        return self._body
-
     @cached_property
     def GET(self):
         return http.QueryDict(self.scope.get("query_string", ""))
