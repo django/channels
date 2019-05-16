ad6fe5a8f7c8c53e285233c8a1349efa62839393
Mario Rial <mrial@datto.com>
2019-05-16 03:21:26 -0400

    reformat file

--- tests/test_generic_http.py
+++ tests/test_generic_http.py
@@ -47,25 +47,23 @@ async def test_async_http_consumer_with_channel_layer():
         Abstract consumer that provides a method that handles running a command and getting a response on a
         device.
         """
-        channel_layer_alias = 'testlayer'
+
+        channel_layer_alias = "testlayer"
 
         async def handle(self, body):
             # Add consumer to a known test group that we will use to send events to.
-            await self.channel_layer.group_add('test_group', self.channel_name)
+            await self.channel_layer.group_add("test_group", self.channel_name)
             await self.send_headers(
-                status=200,
-                headers=[
-                    (b"Content-Type", b"application/json"),
-                ],
+                status=200, headers=[(b"Content-Type", b"application/json")]
             )
 
         async def send_to_long_poll(self, event):
-            received_data = str(event['data']).encode('utf8')
+            received_data = str(event["data"]).encode("utf8")
             # We just echo what we receive, and close the response.
             await self.send_body(received_data, more_body=False)
 
     channel_layers_setting = {
-        'testlayer': {"BACKEND": "channels.layers.InMemoryChannelLayer"}
+        "testlayer": {"BACKEND": "channels.layers.InMemoryChannelLayer"}
     }
 
     with override_settings(CHANNEL_LAYERS=channel_layers_setting):
@@ -84,12 +82,15 @@ async def test_async_http_consumer_with_channel_layer():
         response_start = await communicator.get_response_start(timeout=1)
 
         # Make sure that the start of the response looks good so far.
-        assert response_start['status'] == 200
-        assert response_start['headers'] == [(b'Content-Type', b'application/json')]
+        assert response_start["status"] == 200
+        assert response_start["headers"] == [(b"Content-Type", b"application/json")]
 
         # Send now a message to the consumer through the channel layer. Using the known test_group.
-        channel_layer = get_channel_layer('testlayer')
-        await channel_layer.group_send('test_group', {"type": "send.to.long.poll", "data": "hello from channel layer"})
+        channel_layer = get_channel_layer("testlayer")
+        await channel_layer.group_send(
+            "test_group",
+            {"type": "send.to.long.poll", "data": "hello from channel layer"},
+        )
 
         # Now we should be able to get the message back on the remaining chunk of body.
         body = await communicator.get_body_chunk(timeout=1)
