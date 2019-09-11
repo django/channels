9a37f68b8f1d0744e13f272b760b408cccedabe1
Daniel Hahler <git@thequod.de>
2019-09-11 15:27:17 +0200

    Use allow_thread_sharing for Django < 2.2

--- channels/db.py
+++ channels/db.py
@@ -1,6 +1,9 @@
-from django.db import close_old_connections, connections
+import django
+from django.db import connections
 
 from asgiref.sync import SyncToAsync
+
+HAS_INC_THREAD_SHARING = django.VERSION >= (2, 2)
 
 
 class DatabaseSyncToAsync(SyncToAsync):
@@ -16,12 +19,17 @@ class DatabaseSyncToAsync(SyncToAsync):
 
         This is required for tests using Django's TestCase.
         """
-        from django.db import connections
+        restore_allow_thread_sharing = []
 
         for name in self.main_thread_connections:
             if self.main_thread_connections[name].in_atomic_block:
                 connections[name] = self.main_thread_connections[name]
-                connections[name].inc_thread_sharing()
+                if HAS_INC_THREAD_SHARING:
+                    connections[name].inc_thread_sharing()
+                elif not connections[name].allow_thread_sharing:
+                    restore_allow_thread_sharing.append(name)
+                    connections[name].allow_thread_sharing = True
+        return restore_allow_thread_sharing
 
     def _close_old_connections(self):
         """Like django.db.close_old_connections, but skipping in_atomic_block.
@@ -33,12 +41,14 @@ class DatabaseSyncToAsync(SyncToAsync):
                 conn.close_if_unusable_or_obsolete()
 
     def thread_handler(self, loop, *args, **kwargs):
-        self._inherit_main_thread_connections()
+        restore_allow_thread_sharing = self._inherit_main_thread_connections()
         self._close_old_connections()
         try:
             return super().thread_handler(loop, *args, **kwargs)
         finally:
             self._close_old_connections()
+            for name in restore_allow_thread_sharing:
+                connections[name].allow_thread_sharing = False
 
 
 # The class is TitleCased, but we want to encourage use as a callable/decorator
