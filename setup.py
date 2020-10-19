f7a31c3a22cfe02c54217418c0dc2f06fc803fd6
Carlton Gibson <carlton.gibson@noumenal.es>
2020-10-19 19:14:29 +0200

    Dropped support for Python 3.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     license='BSD',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     install_requires=[
         'Django>=2.2',
         'asgiref~=3.2',
@@ -36,7 +36,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
