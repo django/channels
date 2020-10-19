d9aeb09cbe0c2311c0f2bdd5177dd9f4a036631e
Carlton Gibson <carlton.gibson@noumenal.es>
2020-10-19 19:26:42 +0200

    Add base trove classifier for Django.
    Implying "currently supported versions".

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Framework :: Django',
         'Topic :: Internet :: WWW/HTTP',
     ],
 )
