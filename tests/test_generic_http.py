b285e1e29bba9c599cc607476af59cdf0114fddc
Carlton Gibson <carlton@noumenal.es>
2020-10-22 19:50:15 +0200

    Added as_asgi() consumer classmethod. (#1532)
    To be used when routing a consumer.
    Returns an ASGI v3 single callable that will instantiate the consumer per-scope.
    Closes #1531.

--- tests/test_generic_http.py
+++ tests/test_generic_http.py
@@ -34,3 +34,33 @@ async def test_async_http_consumer():
     assert response["body"] == b'{"value": 42}'
     assert response["status"] == 200
     assert response["headers"] == [(b"Content-Type", b"application/json")]
+
+
+@pytest.mark.asyncio
+async def test_per_scope_consumers():
+    """
+    Tests that a distinct consumer is used per scope, with AsyncHttpConsumer as
+    the example consumer class.
+    """
+
+    class TestConsumer(AsyncHttpConsumer):
+        async def handle(self, body):
+            await self.send_response(
+                200,
+                repr(self).encode("utf-8"),
+                headers={b"Content-Type": b"text/plain"},
+            )
+
+    app = TestConsumer.as_asgi()
+
+    # Open a connection
+    communicator = HttpCommunicator(app, method="GET", path="/test/")
+    response = await communicator.get_response()
+    assert response["status"] == 200
+
+    # And another one.
+    communicator = HttpCommunicator(app, method="GET", path="/test2/")
+    second_response = await communicator.get_response()
+    assert second_response["status"] == 200
+
+    assert response["body"] != second_response["body"]
