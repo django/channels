cc9150835a81b2f921b5481b980182dc99056f42
Carlton Gibson <carlton.gibson@noumenal.es>
2020-10-26 16:06:11 +0100

    Fix RTD build for missing Django dependency.

--- channels/__init__.py
+++ channels/__init__.py
@@ -1,7 +1,11 @@
-import django
-
 __version__ = "2.4.0"
 
-if django.VERSION < (3, 2):
-    default_app_config = "channels.apps.ChannelsConfig"
+try:
+    import django
+
+    if django.VERSION < (3, 2):
+        default_app_config = "channels.apps.ChannelsConfig"
+except ModuleNotFoundError:
+    pass
+
 DEFAULT_CHANNEL_LAYER = "default"
