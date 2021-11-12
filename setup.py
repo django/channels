3416791f8532b7a65c6c5f69b0a5c292a0407075
Michael K <michael-k@users.noreply.github.com>
2021-11-12 17:36:10 +0000

    Run tests against Python 3.10 (#1767)

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Framework :: Django',
         'Topic :: Internet :: WWW/HTTP',
     ],
