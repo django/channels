74ab0d9e724c7354ff90de055daa91873006ad93
Mohammad Javad Naderi <mjnaderi@gmail.com>
2022-04-24 00:27:41 +0430

    Apply daphne hacks only for "runserver" or "daphne" commands
    This improves speed and memory usage of manage.py commands
    other than "runserver". It also improves performance when using
    an alternative ASGI server like "uvicorn" in production.

--- channels/apps.py
+++ channels/apps.py
@@ -1,10 +1,18 @@
-# We import this here to ensure the reactor is installed very early on
-# in case other packages accidentally import twisted.internet.reactor
-# (e.g. raven does this).
-import daphne.server
-from django.apps import AppConfig
+import sys
 
-assert daphne.server  # pyflakes doesn't support ignores
+if (
+    len(sys.argv) >= 2
+    and sys.argv[0].endswith("manage.py")
+    and sys.argv[1] == "runserver"
+) or sys.argv[0].endswith("daphne"):
+    # We import this here to ensure the reactor is installed very early on
+    # in case other packages accidentally import twisted.internet.reactor
+    # (e.g. raven does this).
+    import daphne.server
+
+    assert daphne.server  # pyflakes doesn't support ignores
+
+from django.apps import AppConfig
 
 
 class ChannelsConfig(AppConfig):
--- channels/hacks.py
+++ channels/hacks.py
@@ -1,13 +1,22 @@
+import sys
+
+
 def monkeypatch_django():
     """
     Monkeypatches support for us into parts of Django.
     """
-    # Ensure that the staticfiles version of runserver bows down to us
-    # This one is particularly horrible
-    from django.contrib.staticfiles.management.commands.runserver import (
-        Command as StaticRunserverCommand,
-    )
 
-    from .management.commands.runserver import Command as RunserverCommand
+    if (
+        len(sys.argv) >= 2
+        and sys.argv[0].endswith("manage.py")
+        and sys.argv[1] == "runserver"
+    ):
+        # Ensure that the staticfiles version of runserver bows down to us
+        # This one is particularly horrible
+        from django.contrib.staticfiles.management.commands.runserver import (
+            Command as StaticRunserverCommand,
+        )
 
-    StaticRunserverCommand.__bases__ = (RunserverCommand,)
+        from .management.commands.runserver import Command as RunserverCommand
+
+        StaticRunserverCommand.__bases__ = (RunserverCommand,)
