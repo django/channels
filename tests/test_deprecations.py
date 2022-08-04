e56af6428de7334f9bd77a77053c945af7a07584
Carlton Gibson <carlton.gibson@noumenal.es>
2022-08-04 21:21:12 +0200

    Removed automatic routing of HTTP handler in ProtocolType Router.

--- tests/test_deprecations.py
+++ tests/test_deprecations.py
@@ -1,12 +1,6 @@
 import pytest
 
 from channels.http import AsgiHandler
-from channels.routing import ProtocolTypeRouter
-
-
-def test_automatical_http_protocol_registration_deprecation():
-    with pytest.warns(DeprecationWarning):
-        ProtocolTypeRouter({})
 
 
 def test_asgi_handler_deprecation():
