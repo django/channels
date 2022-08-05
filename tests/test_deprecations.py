9c1ac42f2a5b4dbfcdade02bd52445bab2851cb9
Carlton Gibson <carlton.gibson@noumenal.es>
2022-08-05 09:49:48 +0200

    Removed deprecated AsgiHandler in favour of Django version. (#1893)

--- tests/test_deprecations.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from channels.http import AsgiHandler
-
-
-def test_asgi_handler_deprecation():
-    with pytest.warns(DeprecationWarning, match="AsgiHandler is deprecated"):
-        AsgiHandler()
