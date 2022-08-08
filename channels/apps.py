2c56a172197f334e14a9ed9b78d49e81798a2ab1
Carlton Gibson <carlton.gibson@noumenal.es>
2022-08-08 14:10:33 +0200

    Removed runserver command. (#1897)
    Runserver is added to Daphne in django/daphne#429 allowing users of
    other ASGI servers to use Channels without requiring Daphne and
    Twisted.
    
    To use add daphne to INSTALLED_APPS before django.contrib.staticfiles:
    
    INSTALLED_APPS = [
        "daphne",
        ...
    ]

--- channels/apps.py
+++ channels/apps.py
@@ -1,19 +1,7 @@
-# We import this here to ensure the reactor is installed very early on
-# in case other packages accidentally import twisted.internet.reactor
-# (e.g. raven does this).
-import daphne.server
 from django.apps import AppConfig
-
-assert daphne.server  # pyflakes doesn't support ignores
 
 
 class ChannelsConfig(AppConfig):
 
     name = "channels"
     verbose_name = "Channels"
-
-    def ready(self):
-        # Do django monkeypatches
-        from .hacks import monkeypatch_django
-
-        monkeypatch_django()
