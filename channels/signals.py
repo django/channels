bad57848869d414e2917052f00f85454ed0bbc32
Carlton Gibson <carlton.gibson@noumenal.es>
2022-08-21 15:18:21 +0200

    Removed unused `consumer_started`` and consumer_finished`` signals. (#1907)

--- channels/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db import close_old_connections
-from django.dispatch import Signal
-
-consumer_started = Signal()
-consumer_finished = Signal()
-
-# Connect connection closer to consumer finished as well
-consumer_finished.connect(close_old_connections)
