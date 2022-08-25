707b3968c604308f66080a72807d885dfaa4213d
Carlton Gibson <carlton.gibson@noumenal.es>
2022-08-25 15:14:41 +0200

    Removed pre-Django 3.2 default_app_config fallback.

--- channels/__init__.py
+++ channels/__init__.py
@@ -1,11 +1,4 @@
 __version__ = "3.0.5"
 
-try:
-    import django
-
-    if django.VERSION < (3, 2):
-        default_app_config = "channels.apps.ChannelsConfig"
-except ModuleNotFoundError:
-    pass
 
 DEFAULT_CHANNEL_LAYER = "default"
