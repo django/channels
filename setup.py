53052bdd453188095cca1ac86aed1a822f2bc16e
Michael Käufl <django@c.michael-kaeufl.de>
2022-10-27 15:19:04 +0200

    Run tests against Python 3.11 and add trove classifier

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Framework :: Django",
         "Framework :: Django :: 3",
         "Framework :: Django :: 3.2",
