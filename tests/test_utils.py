68c63fa0ff44f554b8496cf8a3641ecf332ec34f
Seth Foster <fosterbseth@gmail.com>
2022-11-24 15:11:33 -0500

    Add unit test for cancel_callback

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+from channels.utils import await_many_dispatch
+import asyncio
+import async_timeout
+from unittest import mock
+import pytest
+
+
+async def sleep_task(*args):
+    await asyncio.sleep(10)
+
+
+@pytest.mark.asyncio
+async def test_cancel_callback_called():
+    cancel_callback = mock.AsyncMock()
+    with pytest.raises(asyncio.TimeoutError):
+        async with async_timeout.timeout(0):
+            await await_many_dispatch([sleep_task], sleep_task, cancel_callback)
+    assert cancel_callback.called
