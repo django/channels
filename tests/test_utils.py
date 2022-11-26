5358c1e2f7068de214a76805dbb82084ceddb5db
Seth Foster <fosterbseth@gmail.com>
2022-11-25 20:50:08 -0500

    make 3.7 work

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,14 +5,26 @@ from unittest import mock
 import pytest
 
 
+# stub for python 3.7 support
+class AsyncMock(mock.MagicMock):
+    def __init__(self, *args, **kwargs):
+        super(AsyncMock, self).__init__(*args, **kwargs)
+        self.__bool__ = lambda x: True
+    async def __call__(self, *args, **kwargs):
+        return super(AsyncMock, self).__call__(*args, **kwargs)
+
+
 async def sleep_task(*args):
     await asyncio.sleep(10)
 
 
 @pytest.mark.asyncio
 async def test_cancel_callback_called():
-    cancel_callback = mock.AsyncMock()
+    # can replace with mock.AsyncMock after python 3.7 support is dropped
+    cancel_callback = AsyncMock()
     with pytest.raises(asyncio.TimeoutError):
+        # timeout raises asyncio.CancelledError, and await_many_dispatch should
+        # call cancel_callback
         async with async_timeout.timeout(0):
             await await_many_dispatch([sleep_task], sleep_task, cancel_callback)
     assert cancel_callback.called
