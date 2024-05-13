42deaca0e25f5dbb6c5133dc969366b93526960f
Karel Hovorka <hovi6337@gmail.com>
2024-05-13 13:22:18 +0700

    Made WebsocketCommunicator assertions more informative. (#2098)

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -47,6 +47,13 @@ class SimpleWebsocketApp(WebsocketConsumer):
         self.send(text_data=text_data, bytes_data=bytes_data)
 
 
+class AcceptCloseWebsocketApp(WebsocketConsumer):
+    def connect(self):
+        assert self.scope["path"] == "/testws/"
+        self.accept()
+        self.close()
+
+
 class ErrorWebsocketApp(WebsocketConsumer):
     """
     Barebones WebSocket ASGI app for error testing.
@@ -91,6 +98,25 @@ async def test_websocket_communicator():
     assert response == {"hello": "world"}
     # Close out
     await communicator.disconnect()
+
+
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_websocket_incorrect_read_json():
+    """
+    When using an invalid communicator method, an assertion error will be raised with
+    informative message.
+    In this test, the server accepts and then immediately closes the connection so
+    the server is not in a valid state to handle "receive_from".
+    """
+    communicator = WebsocketCommunicator(AcceptCloseWebsocketApp(), "/testws/")
+    await communicator.connect()
+    with pytest.raises(AssertionError) as exception_info:
+        await communicator.receive_from()
+    assert (
+        str(exception_info.value)
+        == "Expected type 'websocket.send', but was 'websocket.close'"
+    )
 
 
 @pytest.mark.django_db
