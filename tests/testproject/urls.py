1cfa985da15b3f3e2d16940a7902d4734f0d07fd
alex <devkral@web.de>
2025-06-11 13:12:55 +0200

    fix linting

--- tests/testproject/urls.py
+++ tests/testproject/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
