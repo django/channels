21b36cf25fe37fcb47b129973cd669985657d9d2
Bona Fide IT GmbH <137388433+BonaFideIT@users.noreply.github.com>
2025-09-24 08:18:22 +0200

    Fix code

--- channels/testing/live.py
+++ channels/testing/live.py
@@ -23,9 +23,6 @@ def make_application(*, static_wrapper):
 def set_database_connection():
     from django.conf import settings
 
-    def set_database_connection():
-    from django.conf import settings
-
     test_db_name = settings.DATABASES["default"]["TEST"]["NAME"]
     if not test_db_name and multiprocessing.get_start_method() == "spawn":
         test_db_name = TEST_DATABASE_PREFIX + settings.DATABASES["default"]["NAME"]
