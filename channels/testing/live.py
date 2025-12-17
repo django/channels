a180692094a920b7851f7f4428cbac8a8b16b8ff
Johannes Wilm <mail@johanneswilm.org>
2025-12-17 14:53:23 +0100

    Add server command queue to ChannelsLiveServerTestCase to allow cleanup commands

--- channels/testing/live.py
+++ channels/testing/live.py
@@ -1,4 +1,5 @@
 from functools import partial
+import multiprocessing
 
 from daphne.testing import DaphneProcess
 from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
@@ -9,6 +10,10 @@ from django.test.testcases import TransactionTestCase
 from django.test.utils import modify_settings
 
 from channels.routing import get_default_application
+
+
+# Global queue for commands from test process to server process
+_server_command_queue = None
 
 
 def make_application(*, static_wrapper):
@@ -28,6 +33,34 @@ def set_database_connection():
     settings.DATABASES["default"]["NAME"] = test_db_name
 
 
+class ServerCommandMiddleware:
+    """
+    Middleware that processes commands from the test process.
+    This is automatically added to the ASGI application in test mode.
+    """
+    def __init__(self, app, commands):
+        self.app = app
+        self.commands = commands
+
+    async def __call__(self, scope, receive, send):
+        # Process any pending server commands before handling the request
+        self.process_server_commands()
+        return await self.app(scope, receive, send)
+
+    def process_server_commands(self):
+        global _server_command_queue
+        if _server_command_queue is None:
+            return
+
+        while not _server_command_queue.empty():
+            try:
+                command = _server_command_queue.get_nowait()
+                if command in self.commands:
+                    self.commands[command]()
+            except:
+                break
+
+
 class ChannelsLiveServerTestCase(TransactionTestCase):
     """
     Does basically the same as TransactionTestCase but also launches a
@@ -40,6 +73,7 @@ class ChannelsLiveServerTestCase(TransactionTestCase):
     ProtocolServerProcess = DaphneProcess
     static_wrapper = ASGIStaticFilesHandler
     serve_static = True
+    commands = {}
 
     @property
     def live_server_url(self):
@@ -51,6 +85,8 @@ class ChannelsLiveServerTestCase(TransactionTestCase):
 
     @classmethod
     def setUpClass(cls):
+        global _server_command_queue
+
         for connection in connections.all():
             if cls._is_in_memory_db(connection):
                 raise ImproperlyConfigured(
@@ -64,8 +100,20 @@ class ChannelsLiveServerTestCase(TransactionTestCase):
         )
         cls._live_server_modified_settings.enable()
 
+        # Create a command queue for communication with the server process
+        _server_command_queue = multiprocessing.Queue()
+        cls._server_command_queue = _server_command_queue
+
+        def make_application_with_middleware(*, static_wrapper):
+            application = get_default_application()
+            # Wrap the application with our command processing middleware
+            application = ServerCommandMiddleware(application, cls.commands)
+            if static_wrapper is not None:
+                application = static_wrapper(application)
+            return application
+
         get_application = partial(
-            make_application,
+            make_application_with_middleware,
             static_wrapper=cls.static_wrapper if cls.serve_static else None,
         )
         cls._server_process = cls.ProtocolServerProcess(
@@ -88,6 +136,13 @@ class ChannelsLiveServerTestCase(TransactionTestCase):
         cls._server_process.join()
         cls._live_server_modified_settings.disable()
         super().tearDownClass()
+
+    def run_server_command(self, command):
+        """
+        Add command to server command queue.
+        """
+        if hasattr(self.__class__, '_server_command_queue'):
+            self._server_command_queue.put(command)
 
     @classmethod
     def _is_in_memory_db(cls, connection):
